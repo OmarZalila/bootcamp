6e292c1ab0b7a32d2aadd0d3d3682e010eeaf825
OmarZalila <omarzalila525@gmail.com>
2024-01-22 22:49:08 +0100

    try to use variables in js file

---
+++ ZALILA_OMAR_JS_CreateVariables.js
@@ -0,0 +1,6 @@
+export function main (){
+    var minAge = 10 ; // the minimum age accepted
+    var minTall = 42 ; // the minimum tall accepted
+    var age ; // the age inputed 
+    var tall ; // the tall inputed
+}

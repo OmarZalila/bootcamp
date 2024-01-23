a5bb6068a090ed6e3cf3a2281cf9a31c86c2289f
OmarZalila <omarzalila525@gmail.com>
2024-01-23 15:51:07 +0100

    condition

---
+++ screenshot/Stretch_Feature .js
@@ -0,0 +1,23 @@
+// Stretch Feature 1 
+var Tall  ;
+var Age ;
+if ( Tall < 42 ){
+    console.log("Sorry kiddo. Maybe next year.");
+    } else if(Age >= 10 ) {
+        console.log("Get on that ride, kiddo!");
+        
+    } else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+
+//Stretch Feature 2    
+var Tall  ;
+var Age ;
+if ( Tall > 42 ){
+    console.log("Get on that ride, kiddo!");
+    } else if(Age >= 10 ) {
+        console.log("Get on that ride, kiddo!");
+        
+    } else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }

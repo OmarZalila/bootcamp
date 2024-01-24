f768c6cf1632038bdf68751ddc4ec8d79aac93e3
OmarZalila <omarzalila525@gmail.com>
2024-01-24 02:19:23 +0100

    App

---
+++ group_activity .js
@@ -0,0 +1,11 @@
+var x=0 ;
+for ( var y = 1996 ; y< 2024 ; y++){
+    console.log(y + " js is awesome");
+    x=x+1 ;
+    if (x % 10 ==0) {
+        console.log("happy decade of js !");
+        y=y+1;
+        x=x+1
+    }
+}
+console.log("HAPPY 28 years of JAVASCRIPT! 🎈🎈🎈");

7c7e4e1f6af82f3d1570cd9341894f5bcd850625
OmarZalila <omarzalila525@gmail.com>
2024-01-24 11:29:37 +0100

    App

--- code.js
+++
@@ -1,7 +0,0 @@
-// case to count number of like 
-var likeCount = 0;
-// function to increase like
-function increaseLikes() {
-    // add like to the case likeCount
-    likeCount = likeCount + 1;
-}
---
+++ code1.js
@@ -0,0 +1,7 @@
+// case to count number of like 
+var likeCount = 0;
+// function to increase like
+function increaseLikes() {
+    // add like to the case likeCount
+    likeCount = likeCount + 1;
+}

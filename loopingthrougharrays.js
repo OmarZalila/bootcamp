b96c4a3b60a03f3ebe6c3121cb168262229cfabf
OmarZalila <omarzalila525@gmail.com>
2024-01-26 00:51:26 +0100

    App

---
+++ loopingthrougharrays.js
@@ -0,0 +1,23 @@
+// #1
+var arr1 = [8,6,7,5,3,0,9];
+for (var i=0 ; i <= arr1.length-1; i++){
+    console.log(arr1[i]);
+}
+
+// #2
+var arr2 = [4,7,13,13,19,37,-2];
+var sum=0 ;
+for (var i=0 ; i <= arr2.length-1; i++){
+    sum = sum + arr2[i] ;
+}
+console.log(sum);
+console.log(i);
+
+//#3
+var arr3 = [6,2,12,14,-24,5,0]; // arr3[1]= "no dice"
+for (var i=0 ; i <= arr3.length-1; i++){
+    if (arr3[i] > 5){
+        console.log(arr3[i]);
+    } else console.log(arr3[i]= "No dice");
+    }
+

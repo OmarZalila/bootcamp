94c12d7770584044b499ab590a806ead4427f775
OmarZalila <omarzalila525@gmail.com>
2024-01-29 22:17:01 +0100

    app

---
+++ screenshot/assignement.js
@@ -0,0 +1,18 @@
+export function main (){
+    var Tall = 45 ;
+    var Age = 11 ; 
+    if ( tall < 42 ){
+        console.log("Sorry kiddo. Maybe next year.");
+        else if(age >= 10 ) {
+
+            console.log( "Get on that ride, kiddo!");
+            else {
+                console.log("Sorry kiddo. Maybe next year.");
+            }
+
+            
+        }
+    }
+
+    }
+
---
+++ work/ZALILA_OMAR_VS_Assignment_2.js
@@ -0,0 +1,14 @@
+console.log("I got a rainbow!");
+console.log("And an extension called Name");
+
+
+function printOdds3to99() {
+    // your code here
+    for(var i=3;i<=99;i++ ){
+        if (i%2 !==0){
+            console.log(i);
+        }
+    }
+}
+
+printOdds3to99();
